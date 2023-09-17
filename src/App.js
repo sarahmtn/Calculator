d995d78891a2641c65ffd5b35fef4bb6fc009b05
sarahmtn <sara.hematian78@gmail.com>
2023-09-17 14:41:49 +0330

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Button from "./components/Button/button";
+import ClickVoice from "./assets/audios/click.mp3";
+import EnterVoice from "./assets/audios/enter.mp3";
 
-function App() {
+const App = () => {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const playAudio = (audio) => new Audio(audio).play();
+
+  const operators = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (val) => {
+    if (operators.includes(val) && calc === "") {
+      return;
+    }
+
+    playAudio(ClickVoice);
+
+    setCalc(calc + val);
+
+    if (!operators.includes(val)) {
+      try {
+        setResult(eval(calc + val).toString());
+      } catch (e) {
+        return;
+      }
+    }
+  };
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+
+    return digits;
+  };
+
+  const calculate = () => {
+    if (calc === "") return setCalc("0");
+
+    playAudio(EnterVoice);
+
+    try {
+      setCalc(eval(calc).toString());
+    } catch (e) {
+      return;
+    }
+  };
+
+  const clearAll = () => {
+    if (calc === "") return;
+    playAudio(EnterVoice);
+
+    setCalc("");
+    setResult("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="outher-edge" />
+        <div className="display">
+          {result ? <span>({result})</span> : ""}
+          {calc || "0"}
+        </div>
+        <div className="operators">
+          <Button handleClick={() => updateCalc("/")} value="/" />
+          <Button handleClick={() => updateCalc("*")} value="x" />
+          <Button handleClick={() => updateCalc("+")} value="+" />
+          <Button handleClick={() => updateCalc("-")} value="-" />
+          <Button _class="clear-btn" handleClick={clearAll} value="C" />
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <Button handleClick={() => updateCalc("0")} value="0" />
+          <Button handleClick={() => updateCalc(".")} value="." />
+          <Button
+            _class="enter-btn"
+            handleClick={() => calculate()}
+            value="="
+          />
+        </div>
+        <div className="outher-edge" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
